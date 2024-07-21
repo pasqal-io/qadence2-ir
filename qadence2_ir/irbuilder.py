327ef33ad5101673d813cd6a49584f15cb304d98
Kaonan Micadiei <kaonan.micadei@pasqal.com>
2024-07-21 23:01:06 +0200

    Define IRBuilder class

---
+++ qadence2_ir/irbuilder.py
@@ -0,0 +1,30 @@
+from __future__ import annotations
+
+from abc import ABC, abstractmethod
+from typing import Generic
+
+from .irast import InputType, Attributes, AST
+from .types import AllocQubits
+
+
+class AbstractIRBuilder(ABC, Generic[InputType]):
+
+    @staticmethod
+    @abstractmethod
+    def set_register(input_obj: InputType) -> AllocQubits:
+        pass
+
+    @staticmethod
+    @abstractmethod
+    def set_directives(input_obj: InputType) -> Attributes:
+        pass
+
+    @staticmethod
+    @abstractmethod
+    def settings(input_obj: InputType) -> Attributes:
+        pass
+
+    @staticmethod
+    @abstractmethod
+    def parse_sequence(input_obj: InputType) -> AST:
+        pass
