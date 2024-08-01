ae4fbc746e4b118ac6174f18f270d1ea961965ba
Kaonan Micadei <k.micadei@gmail.com>
2024-08-01 08:51:09 +0200

    remove unittest from tests

--- tests/test_factory_tools.py
+++ tests/test_factory_tools.py
@@ -1,7 +1,5 @@
 from __future__ import annotations
 
-import unittest
-
 from qadence2_ir.factory_tools import (
     build_instructions,
     extract_inputs_variables,
@@ -12,45 +10,33 @@ from qadence2_ir.irast import AST
 from qadence2_ir.types import Alloc, Assign, Call, Load, QuInstruct, Support
 
 
-class TestFactoryTools(unittest.TestCase):
-    def test_flatten_ast(self) -> None:
-        x = AST.input_variable("x", 1, False)
-        three = AST.numeric(3)
-        div = AST.div(x, three)
-        ast = AST.callable("fn", div, x)
+def test_flatten_ast(classical_ast: AST) -> None:
+    x = AST.input_variable("x", 1, False)
+    three = AST.numeric(3)
+    div = AST.div(x, three)
+    fn = AST.callable("fn", div, x)
 
-        res = list(flatten_ast(ast))
-        self.assertEqual(res, [x, three, div, x, ast])
+    res = list(flatten_ast(classical_ast))
+    assert res == [x, three, div, x, fn]
 
-    def test_filter_ast(self) -> None:
-        x = AST.input_variable("x", 1, False)
-        three = AST.numeric(3)
-        div = AST.div(x, three)
-        ast = AST.callable("fn", div, x)
 
-        res = list(filter_ast(lambda x: x.is_input_variable, ast))
-        self.assertEqual(res, [x, x])
+def test_filter_ast(classical_ast: AST) -> None:
+    x = AST.input_variable("x", 1, False)
 
-    def test_extract_inputs(self) -> None:
-        x = AST.input_variable("x", 1, False)
-        three = AST.numeric(3)
-        div = AST.div(x, three)
-        ast = AST.callable("fn", div, x)
+    res = list(filter_ast(lambda x: x.is_input_variable, classical_ast))
+    assert res == [x, x]
 
-        res = extract_inputs_variables(ast)
-        self.assertEqual(res, {"x": Alloc(1, False)})
 
-    def test_build_instructions(self) -> None:
-        x = AST.input_variable("x", 1, False)
-        three = AST.numeric(3.0)
-        div = AST.div(x, three)
-        fn = AST.callable("fn", div, x)
-        ast = AST.quantum_op("rx", (0,), (1,), fn)
+def test_extract_inputs(classical_ast: AST) -> None:
+    res = extract_inputs_variables(classical_ast)
+    assert res == {"x": Alloc(1, False)}
 
-        res = build_instructions(ast)
-        target = [
-            Assign("%0", Call("div", Load("x"), 3.0)),
-            Assign("%1", Call("fn", Load("%0"), Load("x"))),
-            QuInstruct("rx", Support(target=(0,), control=(1,)), Load("%1")),
-        ]
-        self.assertEqual(res, target)
+
+def test_build_instructions(quantum_ast: AST) -> None:
+    res = build_instructions(quantum_ast)
+    target = [
+        Assign("%0", Call("div", Load("x"), 3.0)),
+        Assign("%1", Call("fn", Load("%0"), Load("x"))),
+        QuInstruct("rx", Support(target=(0,), control=(1,)), Load("%1")),
+    ]
+    assert res == target
