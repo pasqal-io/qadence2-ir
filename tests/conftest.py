e4ed114d706cab4949ac3b436d0ea9b067c294ee
Kaonan Micadei <kaosmicadei@users.noreply.github.com>
2024-08-12 12:39:18 +0200

    [Feature] Create a IRBuilder and ir_compiler_factory (#4)
    * Define an AST for sequence of instructions
    
    * Define IRBuilder class
    
    * Implement ` ir_compile_factory`
    
    * Introduce the `connectivity` option in `AllocQubits` to make it compatible with "crossing-lattice" strategy and `PyQ` gridless model

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,2 +1,28 @@
 # use this file for configuring test fixtures and
 # functions common to every test
+from __future__ import annotations
+
+import pytest
+
+from qadence2_ir.irast import AST
+
+
+@pytest.fixture
+def classical_ast() -> AST:
+    x = AST.input_variable("x", 1, False)
+    three = AST.numeric(3)
+    div = AST.div(x, three)
+    fn = AST.callable("fn", div, x)
+
+    return fn
+
+
+@pytest.fixture
+def quantum_ast() -> AST:
+    x = AST.input_variable("x", 1, False)
+    three = AST.numeric(3)
+    div = AST.div(x, three)
+    fn = AST.callable("fn", div, x)
+    q_op = AST.quantum_op("rx", (0,), (1,), fn)
+
+    return q_op
