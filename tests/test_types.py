2fe0c88c9c485c5cb1206ed6f351f534192fcdee
Pim Venderbosch <pim.venderbosch@pasqal.com>
2024-11-01 14:23:58 +0100

    Add tests for Assign

--- tests/test_types.py
+++ tests/test_types.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from qadence2_ir.types import Alloc
+from qadence2_ir.types import Alloc, Assign
 
 
 def test_alloc_repr() -> None:
@@ -18,3 +18,19 @@ def test_alloc_eq() -> None:
 
     assert create_alloc() == create_alloc()
     assert create_alloc().__eq__([]) is NotImplemented
+
+
+def test_assign_repr() -> None:
+    assert Assign("my-var", 8).__repr__() == "Assign('my-var', 8)"
+    assert Assign("my-var", 3.14).__repr__() == "Assign('my-var', 3.14)"
+    assert (
+        Assign("var*with@non$standard123characters", [3, 2, 1]).__repr__()
+        == "Assign('var*with@non$standard123characters', [3, 2, 1])"
+    )
+
+
+def test_assign_eq() -> None:
+    assert Assign("x", 2) == Assign("x", 2)
+    assert Assign("x", 2) != Assign("y", 2)
+    assert Assign("x", 1) != Assign("x", 34)
+    assert Assign("x", 5).__eq__({}) is NotImplemented
